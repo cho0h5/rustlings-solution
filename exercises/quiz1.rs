66990174a7ee83ed72585d07e1e6517cbbc700a9
Youngho Cho <dominoyh5@gmail.com>
2024-06-17 20:55:14 +0900

    until quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,8 +13,6 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn calculate_price_of_apples {
 
@@ -30,4 +28,12 @@ fn verify_test() {
     assert_eq!(80, price2);
     assert_eq!(41, price3);
     assert_eq!(65, price4);
+}
+
+fn calculate_price_of_apples(number_of_apples: i32) -> i32 {
+    if number_of_apples > 40 {
+        number_of_apples
+    } else {
+        number_of_apples * 2
+    }
 }
